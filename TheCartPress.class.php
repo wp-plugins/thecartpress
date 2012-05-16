e8b5c27cb7f6a3c8e71c89b1605d2230a46a3522
inigoini <inigoini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-16 08:12:20 +0000

    Updating 1.1.9.1
    git-svn-id: https://plugins.svn.wordpress.org/thecartpress/tags/1.1.9.1@544851 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- TheCartPress.class.php
+++ TheCartPress.class.php
@@ -184,7 +184,7 @@ class TheCartPress {
 	 */
 	public function get_setting( $setting_name, $default_value = false ) {
 		$value = isset( $this->settings[$setting_name] ) ? $this->settings[$setting_name ] : $default_value;
-		if ( strlen( $value ) == 0 && $default_value ) $value = $default_value;
+//		if ( is_string( $value ) && strlen( $value ) == 0 && $default_value ) $value = $default_value;
 		$value = apply_filters( 'tcp_get_setting', $value, $setting_name, $default_value );
 		return $value;
 	}
@@ -554,6 +554,7 @@ echo '<br>RES=', count( $res ), '<br>';*/
 	}
 
 	function activate_plugin() {
+		if ( ! session_id() ) session_start();
 		unset( $_SESSION['tcp_session'] );
 		update_option( 'tcp_rewrite_rules', true );
 		global $wp_version;
